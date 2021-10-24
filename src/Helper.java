ecfbeb55b4d44f0ac400f8f9befcf2e46789b061
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2021-10-24 12:54:49 -0500

    Add non-overlapping lengthLongestRepeatedSubstring

--- src/Helper.java
+++ src/Helper.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+//import java.util.HashMap;
 
 public class Helper {
 	
@@ -101,31 +101,77 @@ public class Helper {
 	// 	return length;
 	// }
 
-	static int lengthLongestRepeatedSubstring(String binary)
-	{
+	// static int lengthLongestRepeatedSubstring(String binary)
+	// {
    
-	  // Creating a set to store the last positions of occurrence
-	  HashMap<Character, Integer> seen = new HashMap<>(); 
-	  int maximum_length = 0;
+	//   // Creating a set to store the last positions of occurrence
+	//   HashMap<Character, Integer> seen = new HashMap<>(); 
+	//   int maximum_length = 0;
    
-	  // starting the initial point of window to index 0
-	  int start = 0;
+	//   // starting the initial point of window to index 0
+	//   int start = 0;
    
-	  for(int end = 0; end < binary.length(); end++)
-	  {
-		// Checking if we have already seen the element or not
-		if(seen.containsKey(binary.charAt(end)))
-		{
-		  // If we have seen the number, move the start pointer
-		  // to position after the last occurrence
-		  start = Math.max(start, seen.get(binary.charAt(end)) + 1);
-		}
+	//   for(int end = 0; end < binary.length(); end++)
+	//   {
+	// 	// Checking if we have already seen the element or not
+	// 	if(seen.containsKey(binary.charAt(end)))
+	// 	{
+	// 	  // If we have seen the number, move the start pointer
+	// 	  // to position after the last occurrence
+	// 	  start = Math.max(start, seen.get(binary.charAt(end)) + 1);
+	// 	}
    
-		// Updating the last seen value of the character
-		seen.put(binary.charAt(end), end);
-		maximum_length = Math.max(maximum_length, end-start + 1);
-	  }
-	  return maximum_length;
-	}
+	// 	// Updating the last seen value of the character
+	// 	seen.put(binary.charAt(end), end);
+	// 	maximum_length = Math.max(maximum_length, end-start + 1);
+	//   }
+	//   return maximum_length;
+	// }
+
+
+	// https://www.geeksforgeeks.org/longest-repeating-and-non-overlapping-substring/
+	// Returns the longest repeating non-overlapping
+// substring in str O(n^2)
+	static int lengthLongestRepeatedSubstring(String str) {
+        int n = str.length();
+        int LCSRe[][] = new int[n + 1][n + 1];
+ 
+        String res = ""; // To store result
+        int res_length = 0; // To store length of result
+ 
+        // building table in bottom-up manner
+        int i, index = 0;
+        for (i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                // (j-i) > LCSRe[i-1][j-1] to remove
+                // overlapping
+                if (str.charAt(i - 1) == str.charAt(j - 1)
+                        && LCSRe[i - 1][j - 1] < (j - i)) {
+                    LCSRe[i][j] = LCSRe[i - 1][j - 1] + 1;
+ 
+                    // updating maximum length of the
+                    // substring and updating the finishing
+                    // index of the suffix
+                    if (LCSRe[i][j] > res_length) {
+                        res_length = LCSRe[i][j];
+                        index = Math.max(i, index);
+                    }
+                } else {
+                    LCSRe[i][j] = 0;
+                }
+            }
+        }
+ 
+        // If we have non-empty result, then insert all
+        // characters from first character to last
+        // character of String
+        if (res_length > 0) {
+            for (i = index - res_length + 1; i <= index; i++) {
+                res += str.charAt(i - 1);
+            }
+        }
+ 
+        return res_length;
+    }
 
 }
